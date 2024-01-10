b28af3ed9d16bffaac43ed40112ce2b0f60a36a0
DavidButterton <sadhu.butterton@gmail.com>
2024-01-10 14:57:17 -0800

    added a cancel button to the comment modal, assigned an id selected the element and assigned a varibale for the cancel button, added an event listtener to the button so that when it is clicked is will return the modal backdrop and comment box modal to display None

--- shared.js
+++ shared.js
@@ -1,6 +1,7 @@
 const backdropModal = document.getElementById('backdrop-modal');
 const commentBoxModal = document.getElementById('comment-box__modal');
 const commentButton = document.getElementById('comment-button')
+const commentCancelButton = document.getElementById('comment__cancel-button')
 
 
 commentButton.addEventListener('click', () => {
@@ -8,3 +9,7 @@ commentButton.addEventListener('click', () => {
     commentBoxModal.style.display = 'block';
 })
 
+commentCancelButton.addEventListener('click', () => {
+    backdropModal.style.display = 'None';
+    commentBoxModal.style.display = 'None';
+})

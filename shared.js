176cb289a6b41db2e02edd2eea7f1d30e0059291
DavidButterton <sadhu.butterton@gmail.com>
2024-01-11 01:55:24 -0800

    added a toggle menu bar button with an event listener that displays a side nav bar

--- shared.js
+++ shared.js
@@ -1,8 +1,9 @@
 const backdropModal = document.getElementById('backdrop-modal');
 const commentBoxModal = document.getElementById('comment-box__modal');
-const commentButton = document.getElementById('comment-button')
-const commentCancelButton = document.getElementById('comment__cancel-button')
-
+const commentButton = document.getElementById('comment-button');
+const commentCancelButton = document.getElementById('comment__cancel-button');
+const toggleButtonNav = document.getElementById('toggle-button__sideNav');
+const sideNavBar = document.getElementById('sidebar');
 
 commentButton.addEventListener('click', () => {
     backdropModal.style.display = 'block';
@@ -19,4 +20,8 @@ backdropModal.addEventListener('click', closeModal);
 function closeModal(e) {
     backdropModal.style.display = 'None';
     commentBoxModal.style.display = 'None';
-}
+};
+
+toggleButtonNav.addEventListener('click', () => {
+  sideNavBar.style.display = "flex"
+})
